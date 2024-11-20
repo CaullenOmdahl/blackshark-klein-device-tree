6c547f4457509367054b011b9618149004773288
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-20 18:13:07 +0700

    Added flags to build the image.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
@@ -9,3 +8,20 @@ COMMON_LUNCH_CHOICES := \
     twrp_klein-user \
     twrp_klein-userdebug \
     twrp_klein-eng
+
+# Set the target architecture
+export TARGET_ARCH=arm64
+
+# Set A/B device flag
+export FOX_AB_DEVICE=1
+
+# Set recovery partition locations (if different from defaults)
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/bootdevice/by-name/recovery"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/bootdevice/by-name/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/bootdevice/by-name/vendor"
+
+# Enable LZMA compression for the ramdisk (optional)
+export OF_USE_LZMA_COMPRESSION=1
+
+# Set the default theme for TWRP
+export TW_THEME=portrait_hdpi
