c2c575d8b9771ca7392ac8948b949df8a3c563a5
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-20 17:26:30 +0700

    updated legacy implementation of HAL

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.kona
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_SHARED_LIBRARIES += \
     bootctrl.kona \
     libgptutils \
     libz \
