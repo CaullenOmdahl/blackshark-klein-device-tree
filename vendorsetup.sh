d4432ba3a61c9178ec79842103a86c2c94f08727
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-20 19:56:48 +0700

    Updated exports

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,4 +22,12 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export OF_USE_LZMA_COMPRESSION=1
 
 # Set the default theme for TWRP
-export TW_THEME=portrait_hdpi
+export TW_THEME=portrait_hdpi   
+export COMMON_LUNCH_CHOICES
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/bootdevice/by-name/recovery"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/bootdevice/by-name/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/bootdevice/by-name/vendor"
+export FOX_RECOVERY_BOOT_PARTITION="/dev/block/bootdevice/by-name/boot"
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_PATCH_AVB20=1
