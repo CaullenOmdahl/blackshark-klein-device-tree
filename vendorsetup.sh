01e3a4e1f3679d6ed4369556e7113c282c621d7a
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-20 18:17:26 +0700

    Updated build flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,10 +15,8 @@ export TARGET_ARCH=arm64
 # Set A/B device flag
 export FOX_AB_DEVICE=1
 
-# Set recovery partition locations (if different from defaults)
-export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/bootdevice/by-name/recovery"
-export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/bootdevice/by-name/system"
-export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/bootdevice/by-name/vendor"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export ALLOW_MISSING_DEPENDENCIES=true
 
 # Enable LZMA compression for the ramdisk (optional)
 export OF_USE_LZMA_COMPRESSION=1
